7415adf64b91b547108d508f8189ac55fe73c730
KRouhana <37153041+KRouhana@users.noreply.github.com>
2021-10-13 19:55:11 -0400

    Update ClimbSafeFeatureSet1Controller.java
    First push / First look at my controller feature. Nothing Major.

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet1Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet1Controller.java
@@ -2,15 +2,59 @@ package ca.mcgill.ecse.climbsafe.controller;
 
 import java.sql.Date;
 
+//import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
+import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
+
 public class ClimbSafeFeatureSet1Controller {
+	
+
+	private static ClimbSafe climbSafe; // The system instance
 
   public static void setup(Date startDate, int nrWeeks, int priceOfGuidePerWeek)
-      throws InvalidInputException {}
+      throws InvalidInputException {
+	  
+	 
+	  
+	  
+	  
+	  // climbSafe = ClimbSafeApplication.getClimbSafe();
+	  
+	  
+	  climbSafe.setNrWeeks(nrWeeks);
+	  climbSafe.setStartDate(startDate);
+	  climbSafe.setPriceOfGuidePerWeek(priceOfGuidePerWeek);
+	  
+	  
+	  
+  }
 
+  
+  
+  
+  
+  
   public static void deleteMember(String email) {}
 
   public static void deleteGuide(String email) {}
 
+ 
+  //TODO isValidEmail() 
+  
+  //TODO 
+  private boolean checkValidDate(Date startDate) {
+	  //TODO make it return error
+	  
+	  
+	  return false;
+  }
+  
+  
+  
+  
+  
+  
+  
+  
   // this method needs to be implemented only by teams with seven team members
   public static void deleteHotel(String name) {}
 
