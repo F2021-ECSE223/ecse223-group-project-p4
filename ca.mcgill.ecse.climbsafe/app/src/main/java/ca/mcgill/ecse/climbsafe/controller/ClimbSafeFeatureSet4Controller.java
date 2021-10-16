23d04793ddd04049785b15d896476cee39b830b9
TinetendoWamambo <tinetendo.makata@mail.mcgill.ca>
2021-10-15 21:01:36 -0400

    Update ClimbSafeFeatureSet4Controller.java

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
@@ -1,15 +1,25 @@
 package ca.mcgill.ecse.climbsafe.controller;
 
+// Imports (remove if not used)
+import java.util.List;
+import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
+import ca.mcgill.ecse.climbsafe.model.*;
+import java.util.ArrayList;
+
 public class ClimbSafeFeatureSet4Controller {
 
   public static void addEquipment(String name, int weight, int pricePerWeek)
       
 		  throws InvalidInputException {
 	  
+	 //System into local variables 
+	  
+	  ClimbSafe system = ClimbSafeApplication.getClimbSafe();
+	  
      //Checking for Invalid Input in for name 
 	  
 	  if(name.equals("")){
-		  throw new InvalidInputException("Equipment name cannot be empty");
+		  throw new InvalidInputException("Equipment name cannot be empty.");
 	  }
 
     //User inputs special character in the name, checks each character in the string 
@@ -20,23 +30,23 @@ public class ClimbSafeFeatureSet4Controller {
     	   
     	   if (test < 32) {
     		  
-    		   throw new InvalidInputException("Equipment name cannot have special characters");
+    		   throw new InvalidInputException("Equipment name cannot have special characters.");
     	   
     	   }else if (test == 32 && i == 0) {
     		   
-    		   throw new InvalidInputException("Equipment name cannot start with an empty space");
+    		   throw new InvalidInputException("Equipment name cannot start with an empty space.");
     
                }else if (test > 32 && test < 65) {
     		   
-    		   throw new InvalidInputException("Equipment name cannot have special characters");
+    		   throw new InvalidInputException("Equipment name cannot have special characters.");
     	   
     	   }else if (test > 90 && test < 97) {
     		   
-    		   throw new InvalidInputException("Equipment name cannot have special characters");
+    		   throw new InvalidInputException("Equipment name cannot have special characters.");
     	   
     	   }else if (test > 122) {
     		  
-    		   throw new InvalidInputException("Equipment name cannot have special characters");
+    		   throw new InvalidInputException("Equipment name cannot have special characters.");
     	   }
        }
        
@@ -44,7 +54,7 @@ public class ClimbSafeFeatureSet4Controller {
     
        if (weight <= 0) {
     	   
-    	   throw new InvalidInputException("The weight must be greater than 0 ");    
+    	   throw new InvalidInputException("The weight must be greater than 0.");    
        }
        //TODO the case that there is no weight inputed 
        
@@ -52,11 +62,22 @@ public class ClimbSafeFeatureSet4Controller {
        
        if (pricePerWeek < 0 ) {
     	   
-    	   throw new InvalidInputException("The price per week must be greater than or equal to 0 ");    
+    	   throw new InvalidInputException("The price per week must be greater than or equal to 0.");    
        }
-       //TODO the case that not price per week in inputed 
+       //TODO the case that no price per week in inputed 
        
-   
+   //Checks to see if equipment with same name already exists in the system 
+      
+       BookableItem existingItem = Equipment.getWithName(name);
+       
+       if (existingItem != null) {
+    	   throw new InvalidInputException("A bookable item called" + name + "alreay exists in the system.");    
+       }else {
+    	   //TODO add equipment to BookableItem 
+    	   
+    	//   BookableItem newItem = Equipment.add());
+       }  
+       
   }
 
   public static void updateEquipment(String oldName, String newName, int newWeight,
