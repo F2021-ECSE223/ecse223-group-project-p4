25b9b916bbac634c83e0caaa171bd889e1779fa3
KRouhana <37153041+KRouhana@users.noreply.github.com>
2021-10-19 10:56:35 -0400

    Update ClimbSafeFeatureSet1Controller.java
    Partial code of the feature set 1

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet1Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet1Controller.java
@@ -1,9 +1,11 @@
 package ca.mcgill.ecse.climbsafe.controller;
 
 import java.sql.Date;
+import java.util.List;
 
-//import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
-import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
+//My imports
+import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
+import ca.mcgill.ecse.climbsafe.model.*;
 
 public class ClimbSafeFeatureSet1Controller {
 	
@@ -13,18 +15,19 @@ public class ClimbSafeFeatureSet1Controller {
   public static void setup(Date startDate, int nrWeeks, int priceOfGuidePerWeek)
       throws InvalidInputException {
 	  
-	 
+	
 	  
-	  
-	  
-	  // climbSafe = ClimbSafeApplication.getClimbSafe();
-	  
+	  checkValidDate(startDate);
+	  	  
+	  if(nrWeeks <= 0) throw new InvalidInputException("The price of guide per week must be greater than or equal to zero");
+	  if(priceOfGuidePerWeek <= 0) throw new InvalidInputException("The price of guide per week must be greater than or equal to zero");
+
+
+	  climbSafe = ClimbSafeApplication.getClimbSafe();
 	  
 	  climbSafe.setNrWeeks(nrWeeks);
 	  climbSafe.setStartDate(startDate);
 	  climbSafe.setPriceOfGuidePerWeek(priceOfGuidePerWeek);
-	  
-	  
 	  
   }
 
@@ -33,19 +36,51 @@ public class ClimbSafeFeatureSet1Controller {
   
   
   
-  public static void deleteMember(String email) {}
+  public static void deleteMember(String email) {
+	  //check if member exists or no 
+	  //check if instance of guide
+	  //check if email is valid
+	  
+	  
+	  
+  }
 
-  public static void deleteGuide(String email) {}
+  public static void deleteGuide(String email) {
+	  
+	  
+	  
+  }
 
  
   //TODO isValidEmail() 
   
-  //TODO 
-  private boolean checkValidDate(Date startDate) {
-	  //TODO make it return error
+    private static void checkValidDate(Date startDate) throws InvalidInputException{
+	 
+	  int [] monthWith30Days = {3,5,8,10};
 	  
 	  
-	  return false;
+	  if(startDate.getYear() < 2021) throw new InvalidInputException("Invalide date"); //If year before 2021
+	  
+	  if(startDate.getDate() < 1 || startDate.getDate() > 31) throw new InvalidInputException("Invalide date"); //If date is less than 0 or more than 31
+
+	  if(startDate.getMonth() < 0 || startDate.getMonth() > 11 ) throw new InvalidInputException("Invalide date"); //If month is less than 0 or more than 11 (Date class takes it like this)
+	
+	  
+	  //Checks for month with 30 days has 31 days in it
+	  for(int i=0; i < monthWith30Days.length; i++) {
+		  if(startDate.getMonth() == monthWith30Days[i]) {
+			  if(startDate.getDate() == 30) throw new InvalidInputException("Invalide date");
+		  }
+	  }
+	  
+	  //Checks for February
+	  if(startDate.getMonth() == 1) {
+		  
+		  if(startDate.getDate() > 29) throw new InvalidInputException("Invalide date"); // If has more than 29 days
+
+		  if(startDate.getDate() == 28 && startDate.getYear() % 4 == 0 ) throw new InvalidInputException("Invalide date"); //If leap year and has 28 days error 
+
+	  }
   }
   
   
