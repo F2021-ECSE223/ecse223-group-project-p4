37128b470c64f35ef7b71830f10805da224c1b91
Matthieuhakim <77667703+Matthieuhakim@users.noreply.github.com>
2021-10-20 12:38:13 -0400

    Update ClimbSafeFeatureSet5Controller.java
    updated addEquipmentBundle()

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet5Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet5Controller.java
@@ -2,51 +2,53 @@ package ca.mcgill.ecse.climbsafe.controller;
 
 import java.util.List;
 
+import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
+import ca.mcgill.ecse.climbsafe.model.*;
+
 public class ClimbSafeFeatureSet5Controller {
 	
-  public static void addEquipmentBundle(String name, int discount, List<String> equipmentNames,
+	
+	
+	private static ClimbSafe system = ClimbSafeApplication.getClimbSafe(); // The system instance
+
+	
+	public static void addEquipmentBundle(String name, int discount, List<String> equipmentNames,
       List<Integer> equipmentQuantities) throws InvalidInputException {
 	  
-	  
+		
 	  if(name ==  "") {
 		  //TODO: make string full of spaces invalid too
 		  throw new InvalidInputException("Equipment bundle name cannot be empty");
-	  }else if(name == "an already existing item") {
 		  
-		  //TODO: for_loop the system to find name == bookableItems.getName(); (something like that)
+		  
+	  }else if(BookableItem.hasWithName(name)) {
 		  
 		  throw new InvalidInputException("A bookable item called " + name + " already exists");
+		  
 	  }
-	  else {
-		  //TODO: name is valid, assign to bundle
-	  }
+	  //name is now valid
 	  
 	  
 	  
 	  
 	  if(discount < 0) {
-		  
 		  throw new InvalidInputException("Discount must be at least 0");
+		  
 	  }else if(discount > 100) {
 		  throw new InvalidInputException("Discount must be no more than 100");
-		  
-	  }else {
-		  
-		  //TODO: discount is valid, assign to bundle
 	  }
+	  //discount is now valid
 	  
 	  
 	  
-	  //TODO: Do not count duplicates in the size of list
-	  //TODO: Ask what we should do if there is a duplicate equipment but size constraint is satisfied (do we add another quantity?)
 	  
-	  if(equipmentNames.size() < 2) {
-		  
-		  throw new InvalidInputException("Equipment bundle must contain at least two distinct types of equipment");
+	  for(String equipmentName : equipmentNames) {
+		  if(!Equipment.hasWithName(equipmentName) || (Equipment.getWithName(equipmentName) instanceof Equipment)) {
+			  
+			  throw new InvalidInputException("Equipment " + equipmentName + " does not exist");
+		  }
 	  }
-	  //TODO: Check that all names in equipmentNames have a corresponding equipment
-	  //throw new InvalidInputException("Equipment " + equipmentName + " does not exist");
-	  
+	  //all names have a corresponding equipment
 	  
 	  
 	  if(equipmentQuantities.size() == equipmentNames.size()) {
@@ -60,17 +62,49 @@ public class ClimbSafeFeatureSet5Controller {
 		  
 	  }else {
 		  
-		  //TODO: Ask what happens if names and quantities do not have the same size
-		  //throw new InvalidInputException("Each bundle item must have quantity greater than or equal to 1");
+		  throw new InvalidInputException("Each bundle item must have quantity greater than or equal to 1");
 	  }
-		  
-		  
 	  
-	  //TODO: Add Bundle with name, discount, equipments and quantities
+	  
+	  
+	  //TODO: Do not count duplicates in the size of list
+	  //TODO: Ask what we should do if there is a duplicate equipment but size constraint is satisfied (do we add another quantity?)
+	  
+	  
+	  if(equipmentNames.size() < 2) {
+		  
+		  throw new InvalidInputException("Equipment bundle must contain at least two distinct types of equipment");
+	  }
+	  
+	  boolean hasTwoDifferentEquipments = false;
+	  for(int i = 0; i < equipmentNames.size(); i++) {
+		  
+		  if(equipmentNames.get(i) != equipmentNames.get(0)) {
+			  
+			  hasTwoDifferentEquipments = true;
+		  }
+	  }
+	  if(!hasTwoDifferentEquipments) {
+		  throw new InvalidInputException("Equipment bundle must contain at least two distinct types of equipment");
+
+	  }
+	 //There are at least 2 distinct types of equipment
+	  
+	  
+	  	  
+
+	  EquipmentBundle newBundle = new EquipmentBundle(name, discount, system);
+	  for(int i = 0; i < equipmentQuantities.size(); i++) {
+		  
+		  BundleItem item = new BundleItem(equipmentQuantities.get(i), system, newBundle, (Equipment)(BookableItem.getWithName(equipmentNames.get(i))));
+		  newBundle.addBundleItem(item);
+	  }
+	  system.addBundle(newBundle);
+	  //Added Bundle with name, discount, bundle items and quantities
 	  
   }
 
-  public static void updateEquipmentBundle(String oldName, String newName, int newDiscount,
+	public static void updateEquipmentBundle(String oldName, String newName, int newDiscount,
       List<String> newEquipmentNames, List<Integer> newEquipmentQuantities)
       throws InvalidInputException {}
 
