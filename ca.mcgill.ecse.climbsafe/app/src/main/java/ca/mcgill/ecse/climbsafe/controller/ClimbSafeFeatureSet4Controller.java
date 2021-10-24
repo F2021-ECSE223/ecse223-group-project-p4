db701c6dea450088b73863b672c431fa94fe4c6a
TinetendoWamambo <tinetendo.makata@mail.mcgill.ca>
2021-10-24 11:41:41 -0400

    javadoc

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
@@ -6,26 +6,38 @@ import ca.mcgill.ecse.climbsafe.model.*;
 
 
 public class ClimbSafeFeatureSet4Controller {
-
+/**
+ * @author Tinetendo Makata
+ * @param name
+ * @param weight
+ * @param pricePerWeek
+ * @throws InvalidInputException
+ * This method creates a new piece of equipment in the ClimbSafe system with a unique name, a weight, and a price per week 
+ * If the input for the piece is not correct, then the method outputs an invalid input error
+ */
   public static void addEquipment(String name, int weight, int pricePerWeek)
       
 		  throws InvalidInputException {
     	   
 	  checkCommonConditions(weight, name, pricePerWeek);
 	  
-	//Checks to see if equipment with same name already exists in the system 
+	//Checks to see if equipment with same name already exists in the system and outputs an error if that is the case 
       
       BookableItem existingItem = BookableItem.getWithName(name);
       
       if (existingItem != null) {
+    	  
    	   if(existingItem instanceof Equipment) 
+   		   
    		   throw new InvalidInputException("The piece of equipment already exists");    
+   	   
    	   if(existingItem instanceof EquipmentBundle)
    		   throw new InvalidInputException("The equipment bundle already exists");    
       }
       
 	  ClimbSafe system = ClimbSafeApplication.getClimbSafe();
 	  
+	  //add the equipment item to the system if it passes all the previous tests 
 	  system.addEquipment(new Equipment (name, weight, pricePerWeek, system));
       
   }
@@ -37,10 +49,12 @@ public class ClimbSafeFeatureSet4Controller {
 	  
 	  BookableItem toChange = BookableItem.getWithName(oldName);
 	  
+	  //Checks to see if the item to change exists in the system and that it is not a bundle 
+	  
 	  if (toChange == null || toChange instanceof EquipmentBundle)
 		 throw new InvalidInputException("That equipment does not yet exist in the system.");	 
 	 
-	  //Checks to see if equipment with same name already exists in the system 
+	  //Checks to see if equipment or bundle with same name already exists in the system 
       
       BookableItem existingItem = BookableItem.getWithName(newName);
       
@@ -51,13 +65,20 @@ public class ClimbSafeFeatureSet4Controller {
    		   throw new InvalidInputException("An equipment bundle with the same name already exists");    
       }
 	 
-   	   //Updating the equipment
+   	   //Updating the equipment by setting a new name, weight and price per week 
    	   toChange.setName(newName);
    	   ((Equipment) toChange).setWeight(newWeight);
    	   ((Equipment) toChange).setPricePerWeek(newPricePerWeek);
       
   }
-  
+  /** 
+   *This method checks the common conditions that are tested whether the item is being added or updated 
+   *@author Tinetendo Makata
+   * @param weight
+   * @param name
+   * @param pricePerWeek
+   * @throws InvalidInputException
+   */
   private static void checkCommonConditions(int weight, String name, int pricePerWeek) throws InvalidInputException{
 	  
 	  //Checking for Invalid Input in for name 
@@ -77,7 +98,6 @@ public class ClimbSafeFeatureSet4Controller {
     	   
     	   throw new InvalidInputException("The price per week must be greater than or equal to 0");    
        }
-    		   
-  }
+    }
 
 }
