657fc71371c70dcd3bdf1c5221e45740490ffeb2
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2021-10-31 19:08:47 -0400

    State machine preliminary draft

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
@@ -1,5 +1,25 @@
 package ca.mcgill.ecse.climbsafe.controller;
 
 public class AssignmentController {
+  
+     public static void initiateAssignment() {
+       //If banned throw exception
+     }
+     
+     public static void payForTrip() {
+       //If banned throw exception
+     }
+     
+     public static void startAllTrips() {
+       //If banned throw exception
+     }
+     
+     public static void finishTrip() {
+       //If banned throw exception
+     }
+     
+     public static void cancelTrip() {
+       //If banned throw exception
+     }
 
 }
