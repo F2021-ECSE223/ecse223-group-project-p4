c8e8c98d523a4a0efd9c2723468515abc385c776
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2021-11-01 20:11:05 -0400

    Step definitions prelim draft

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
@@ -2,23 +2,23 @@ package ca.mcgill.ecse.climbsafe.controller;
 
 public class AssignmentController {
   
-     public static void initiateAssignment() {
+     public static void initiateAssignment() throws InvalidInputException {
        //If banned throw exception
      }
      
-     public static void payForTrip() {
+     public static void payForTrip(String memberEmail, String authorizationCode) throws InvalidInputException{
        //If banned throw exception
      }
      
-     public static void startAllTrips() {
+     public static void startTrips(int weekNr) throws InvalidInputException{
        //If banned throw exception
      }
      
-     public static void finishTrip() {
+     public static void finishTrip(String memberEmail) throws InvalidInputException{
        //If banned throw exception
      }
      
-     public static void cancelTrip() {
+     public static void cancelTrip(String memberEmail) throws InvalidInputException{
        //If banned throw exception
      }
 
--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/model/ClimbSafe.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/model/ClimbSafe.java
@@ -1,4 +1,4 @@
-/*PLEASE DO NOT EDIT THIS CODE*/
+F/*PLEASE DO NOT EDIT THIS CODE*/
 /*This code was generated using the UMPLE 1.31.1.5860.78bb27cc6 modeling language!*/
 
 package ca.mcgill.ecse.climbsafe.model;
