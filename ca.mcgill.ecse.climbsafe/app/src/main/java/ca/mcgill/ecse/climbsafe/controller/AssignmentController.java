6d4c060db4f20624b7be417258bad48a4fab7e55
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2021-11-03 19:17:11 -0400

    Update AssignmentController.java

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
@@ -2,22 +2,47 @@ package ca.mcgill.ecse.climbsafe.controller;
 
 public class AssignmentController {
 
+  /**
+   * @author Adam Kazma, Ralph Nassar
+   * @throws InvalidInputException
+   */
   public static void initiateAssignment() throws InvalidInputException {
     //If banned throw exception
   }
 
+  /**
+   * @author Tinetendo Makata
+   * @param memberEmail
+   * @param authorizationCode
+   * @throws InvalidInputException
+   */
   public static void payForTrip(String memberEmail, String authorizationCode) throws InvalidInputException{
     //If banned throw exception
   }
 
+  /**
+   * @author Wassim Jabbour
+   * @param weekNr
+   * @throws InvalidInputException
+   */
   public static void startTrips(int weekNr) throws InvalidInputException{
     //If banned throw exception
   }
 
+  /**
+   * @author Matthieu Hakim
+   * @param memberEmail
+   * @throws InvalidInputException
+   */
   public static void finishTrip(String memberEmail) throws InvalidInputException{
     //If banned throw exception
   }
 
+  /**
+   * @author Karl Rouhana
+   * @param memberEmail
+   * @throws InvalidInputException
+   */
   public static void cancelTrip(String memberEmail) throws InvalidInputException{
     //If banned throw exception
   }
