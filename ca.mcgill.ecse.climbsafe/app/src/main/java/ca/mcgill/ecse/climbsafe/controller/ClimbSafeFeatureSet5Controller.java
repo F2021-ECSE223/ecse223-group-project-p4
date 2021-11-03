0c5e767c4e7dda7e67ecb06542165cb05b3cfa22
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2021-11-03 19:00:51 -0400

    Fixing merge issues

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet5Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet5Controller.java
@@ -1,14 +1,239 @@
 package ca.mcgill.ecse.climbsafe.controller;
 
-import java.util.List;
+import java.util.*;
+
+import ca.mcgill.ecse.climbsafe.application.*;
+import ca.mcgill.ecse.climbsafe.model.*;
 
 public class ClimbSafeFeatureSet5Controller {
 
-  public static void addEquipmentBundle(String name, int discount, List<String> equipmentNames,
-      List<Integer> equipmentQuantities) throws InvalidInputException {}
 
+
+  /**
+   * 
+   * The addEquipmentBundle method creates a new bundle with specified name, discount, equipment
+   * items and their quantities The method throws an InvalidInputException whenever any of the input
+   * is invalid
+   * 
+   * 
+   * @author Matthieu Hakim
+   * @param name - The name of the new bundle
+   * @param discount - The discount for the bundle
+   * @param equipmentNames - The list of names for all the desired equipment items
+   * @param equipmentQuantities - The list of quantities for each equipment item
+   * @throws InvalidInputException - throws invalid input exception if there's an error
+   * 
+   */
+  public static void addEquipmentBundle(String name, int discount, List<String> equipmentNames,
+      List<Integer> equipmentQuantities) throws InvalidInputException {
+
+    ClimbSafe system = ClimbSafeApplication.getClimbSafe(); // The system instance
+
+    if (equipmentNames.size() < 2) {
+      throw new InvalidInputException(
+          "Equipment bundle must contain at least two distinct types of equipment");
+    }
+    // There are at least 2 equipment items
+
+    if (name.trim().isEmpty()) {
+      throw new InvalidInputException("Equipment bundle name cannot be empty");
+    } else if (BookableItem.hasWithName(name)) {
+      throw new InvalidInputException("A bookable item called " + name + " already exists");
+    }
+    // name is now valid
+
+    if (discount < 0) {
+      throw new InvalidInputException("Discount must be at least 0");
+    } else if (discount > 100) {
+      throw new InvalidInputException("Discount must be no more than 100");
+    }
+    // discount is now valid
+
+    for (String equipmentName : equipmentNames) {
+      if (!Equipment.hasWithName(equipmentName)
+          || !(Equipment.getWithName(equipmentName) instanceof Equipment)) {
+        throw new InvalidInputException("Equipment " + equipmentName + " does not exist");
+      }
+    }
+    // all names have a corresponding equipment
+
+
+    if (equipmentQuantities.size() == equipmentNames.size()) {
+
+      for (Integer equipmentQuantity : equipmentQuantities) {
+        if (equipmentQuantity <= 0) {
+          throw new InvalidInputException(
+              "Each bundle item must have quantity greater than or equal to 1");
+        }
+      }
+
+    } else {
+      throw new InvalidInputException(
+          "Each bundle item must have quantity greater than or equal to 1");
+    }
+    // Each item has a corresponding quantity
+
+
+    boolean hasTwoDifferentEquipments = false;
+
+    for (int i = 1; i < equipmentNames.size(); i++) {
+      if (!equipmentNames.get(0).equals(equipmentNames.get(i))) {
+        hasTwoDifferentEquipments = true;
+      }
+    }
+    if (!hasTwoDifferentEquipments) {
+      throw new InvalidInputException(
+          "Equipment bundle must contain at least two distinct types of equipment");
+    }
+    // There are at least 2 distinct types of equipment
+
+    EquipmentBundle newBundle = new EquipmentBundle(name, discount, system);
+
+    for (int i = 0; i < equipmentNames.size(); i++) {
+      BundleItem item = new BundleItem(equipmentQuantities.get(i), system, newBundle,
+          (Equipment) (BookableItem.getWithName(equipmentNames.get(i))));
+      newBundle.addBundleItem(item);
+    }
+    system.addBundle(newBundle);
+    // Added Bundle with name, discount, bundle items and quantities
+
+  }
+
+
+
+  /**
+   * 
+   * The updateEquipmentBundle method modifies an existing bundle and changes its name, discount,
+   * equipment items and their quantities The method throws an InvalidInputException whenever any of
+   * the input is invalid
+   * 
+   * 
+   * @author Matthieu Hakim
+   * @param oldName - The name of the bundle to be updated
+   * @param newName - The new name of the bundle
+   * @param newDiscount - The new discount for the bundle
+   * @param newEquipmentNames - The new list of names for all the desired equipment items
+   * @param newEquipmentQuantities - The new list of quantities for each equipment item
+   * @throws InvalidInputException - throws invalid input exception if there's an error
+   * 
+   */
   public static void updateEquipmentBundle(String oldName, String newName, int newDiscount,
       List<String> newEquipmentNames, List<Integer> newEquipmentQuantities)
-      throws InvalidInputException {}
+      throws InvalidInputException {
+
+    ClimbSafe system = ClimbSafeApplication.getClimbSafe(); // The system instance
+
+    if (newEquipmentNames.size() < 2) {
+      throw new InvalidInputException(
+          "Equipment bundle must contain at least two distinct types of equipment");
+    }
+    // There are at least 2 equiments
+
+    if (!BookableItem.hasWithName(oldName)
+        || !(BookableItem.getWithName(oldName) instanceof EquipmentBundle)) {
+      throw new InvalidInputException("Equipment bundle " + oldName + " does not exist");
+    } // equipment bundle exists
+
+    if (newDiscount < 0) {
+      throw new InvalidInputException("Discount must be at least 0");
+    } else if (newDiscount > 100) {
+      throw new InvalidInputException("Discount must be no more than 100");
+    }
+    // discount is now valid
+
+
+
+    for (String equipmentName : newEquipmentNames) {
+      if (!Equipment.hasWithName(equipmentName)
+          || !(Equipment.getWithName(equipmentName) instanceof Equipment)) {
+        throw new InvalidInputException("Equipment " + equipmentName + " does not exist");
+      }
+    }
+    // all names have a corresponding equipment
+
+
+    if (newEquipmentQuantities.size() == newEquipmentNames.size()) {
+      for (Integer equipmentQuantity : newEquipmentQuantities) {
+        if (equipmentQuantity <= 0) {
+          throw new InvalidInputException(
+              "Each bundle item must have quantity greater than or equal to 1");
+        }
+      }
+    } else {
+      throw new InvalidInputException(
+          "Each bundle item must have quantity greater than or equal to 1");
+    }
+    // Each item has a corresponding quantity
+
+
+    boolean hasTwoDifferentEquipments = false;
+    for (int i = 1; i < newEquipmentNames.size(); i++) {
+      if (!newEquipmentNames.get(0).equals(newEquipmentNames.get(i))) {
+        hasTwoDifferentEquipments = true;
+      }
+    }
+    if (!hasTwoDifferentEquipments) {
+      throw new InvalidInputException(
+          "Equipment bundle must contain at least two distinct types of equipment");
+    }
+    // There are at least 2 distinct types of equipment
+
+    if (!oldName.equals(newName)) {
+      if (newName.trim().isEmpty()) {
+        throw new InvalidInputException("Equipment bundle name cannot be empty");
+      } else if (BookableItem.hasWithName(newName)) {
+        throw new InvalidInputException("A bookable item called " + newName + " already exists");
+      }
+    } // new name is valid
+
+    // The bundle we want to update
+    EquipmentBundle bundle = (EquipmentBundle) BookableItem.getWithName(oldName);
+
+    // update the name
+    bundle.setName(newName);
+
+    // update the discount
+    bundle.setDiscount(newDiscount);
+
+    ArrayList<BundleItem> oldItemsToRemove = new ArrayList<BundleItem>();
+
+    for (BundleItem item : bundle.getBundleItems()) {
+      oldItemsToRemove.add(item);
+    }
+
+    for (int i = 0; i < newEquipmentNames.size(); i++) {
+
+      Equipment equipment = (Equipment) BookableItem.getWithName(newEquipmentNames.get(i));
+      boolean found = false;
+
+      for (BundleItem bundleItem : equipment.getBundleItems()) {
+        if (bundleItem.getBundle() == bundle) {
+
+          // when the item is the same for the old and new bundle
+          bundleItem.setQuantity(newEquipmentQuantities.get(i));
+          found = true;
+
+          oldItemsToRemove.remove(bundleItem);
+          break;
+
+        }
+      }
+
+      if (!found) {
+
+        // if new item was not in old bundle
+        bundle.addBundleItem(newEquipmentQuantities.get(i), system, equipment);
+      }
+
+    }
+
+    for (BundleItem bundleItem : oldItemsToRemove) {
+      bundleItem.delete();
+    }
+    // Delete items that are not in the new bundle
+
+    // Updated Bundle with name, discount, bundle items and quantities
+
+  }
 
 }
--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet6Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet6Controller.java
@@ -1,14 +1,172 @@
 package ca.mcgill.ecse.climbsafe.controller;
 
+// Default imports
 import java.util.List;
+
+// My imports
+import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
+import ca.mcgill.ecse.climbsafe.model.*;
+import java.util.ArrayList;
 
 public class ClimbSafeFeatureSet6Controller {
 
-  public static void deleteEquipment(String name) throws InvalidInputException {}
+  /**
+   * Deletes a piece of equipment with a given name from the system (If it exists)
+   * 
+   * @author Wassim Jabbour
+   * @param name The name of the piece of equipment to delete from the system
+   * @throws InvalidInputException It throws an invalid input exception if the piece of equipment
+   *         with the given name is part of a bundle
+   */
+  public static void deleteEquipment(String name) throws InvalidInputException {
 
-  // this method does not need to be implemented by a team with five team members
-  public static void deleteEquipmentBundle(String name) {}
+    // Retrieving the item to delete from the system
+    BookableItem toDelete = BookableItem.getWithName(name);
 
-  public static List<TOAssignment> getAssignments() {}
+    // Checking that it is actually of type equipment
+    if (toDelete != null && toDelete instanceof Equipment equipmentItem) {
+
+      // Checking that the equipment with the indicated name is not part of a bundle
+      if (equipmentItem.getBundleItems().size() != 0)
+        throw (new InvalidInputException(
+            "The piece of equipment is in a bundle and cannot be deleted"));
+
+      // Deleting the piece of equipment
+      toDelete.delete();
+    }
+  }
+
+  /**
+   * Deletes an equipment bundle with a given name from the system (If it exists)
+   * 
+   * @author Wassim Jabbour
+   * @param name The name of the equipment bundle to delete from the system
+   */
+  public static void deleteEquipmentBundle(String name) {
+
+    // Retrieving the bundle to delete from the system
+    var toDelete = BookableItem.getWithName(name);
+
+    // Delete the bundle if it exists in the system
+    if (toDelete != null && toDelete instanceof EquipmentBundle)
+      toDelete.delete();
+
+  }
+
+  /**
+   * Returns all the assignments a system has in the form of transfer objects
+   * 
+   * @author Wassim Jabbour
+   */
+  public static List<TOAssignment> getAssignments() {
+
+    // Putting the system we're working on in a local variable
+    ClimbSafe system = ClimbSafeApplication.getClimbSafe();
+
+    // Retrieving the list of all the assignments of the system
+    List<Assignment> assignments = system.getAssignments();
+
+    // Copying all the elements of the list to a new list of TOAssignments
+    List<TOAssignment> TOassignments = new ArrayList<>();
+
+    for (Assignment assignment : assignments) {
+
+      // Extracting the member email
+      String memberEmail = assignment.getMember().getEmail();
+
+      // Extracting the member name
+      String memberName = assignment.getMember().getName();
+
+      // Extracting the guide name and email
+      String guideEmail, guideName;
+      if (assignment.getGuide() != null) {
+        guideEmail = assignment.getGuide().getEmail();
+        guideName = assignment.getGuide().getName();
+      } else {
+        guideEmail = null;
+        guideName = null;
+      }
+
+      // Extracting the hotel name
+      String hotelName;
+      if (assignment.getHotel() != null)
+        hotelName = assignment.getHotel().getName();
+      else
+        hotelName = null;
+
+      // Extracting the assignment start week
+      int startWeek = assignment.getStartWeek();
+
+      // Extracting the assignment end week
+      int endWeek = assignment.getEndWeek();
+
+      // Calculating the total cost for the guide
+      int totalCostForGuide;
+      if (guideEmail != null)
+        totalCostForGuide = (endWeek - startWeek + 1) * system.getPriceOfGuidePerWeek();
+      else
+        totalCostForGuide = 0;
+
+      // Calculating the total cost of the booked equipment using a helper method
+      int totalCostForEquipment = getTotalCostOfEquipment(assignment);
+
+      // Adding the corresponding TOAssignment to the list we will be returning
+      TOassignments.add(new TOAssignment(memberEmail, memberName, guideEmail, guideName, hotelName,
+          startWeek, endWeek, totalCostForGuide, totalCostForEquipment));
+    }
+
+    return TOassignments; // Returning a list of TOAssignments
+  }
+
+  /**
+   * Helper method that computes the total price of the equipment booked by a member. It accounts
+   * for the length of the trip and the number of booked items. In the case of booking a bundle, it
+   * also applies a discount if a guide was requested
+   * 
+   * @author Wassim Jabbour
+   * @param assignment The assignment we want to compute the total cost of the equipment for
+   */
+  private static int getTotalCostOfEquipment(Assignment assignment) {
+
+    // Extracting the attributes of the assignment
+    int startWeek = assignment.getStartWeek();
+    int endWeek = assignment.getEndWeek();
+    boolean hasGuide = (assignment.getGuide() != null);
+
+    // Variable that keeps track of the total cost throughout the method
+    int totalPrice = 0;
+
+    // Looping over all the booked items of the member
+    for (var bookedItem : assignment.getMember().getBookedItems()) {
+
+      // If it is an equipment item
+      if (bookedItem.getItem()instanceof Equipment item)
+        // Multiplying by the number of weeks and the quantity
+        totalPrice += bookedItem.getQuantity() * item.getPricePerWeek() * (endWeek - startWeek + 1);
+
+      // If it is an equipment bundle
+      else {
+        int bundlePricePerWeek = 0;
+        EquipmentBundle bundle = (EquipmentBundle) bookedItem.getItem();
+
+        // Looping over all the items in the bundle
+        for (BundleItem bundleItem : bundle.getBundleItems()) {
+          int itemPricePerWeek =
+              bundleItem.getEquipment().getPricePerWeek() * bundleItem.getQuantity();
+          bundlePricePerWeek += itemPricePerWeek;
+        }
+
+        // Applying the discount if a guide was hired
+        if (hasGuide)
+          bundlePricePerWeek =
+              (int) Math.round(bundlePricePerWeek * (100 - bundle.getDiscount()) / 100.0);
+
+        // Adding the price of this bundle by multiplying by the quantity and the number of weeks
+        totalPrice += bookedItem.getQuantity() * bundlePricePerWeek * (endWeek - startWeek + 1);
+      }
+    }
+
+    return totalPrice; // Returning the total price over the trip
+  }
 
 }
