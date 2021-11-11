4fd4a20a86cf5b31f552567ab3e083941698ce8a
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2021-11-11 17:51:47 -0500

    Revert "Merge branch 'main' of https://github.com/F2021-ECSE223/ecse223-group-project-p4"
    This reverts commit 8b08c601d22d06b9beac74a9ccf36986e40fe44f, reversing
    changes made to 586c60161c17bd864ebe52b4226b57d49ece1353.

--- ca.mcgill.ecse.climbsafe/app/src/test/java/ca/mcgill/ecse/climbsafe/features/AssignmentFeatureStepDefinitions.java
+++ ca.mcgill.ecse.climbsafe/app/src/test/java/ca/mcgill/ecse/climbsafe/features/AssignmentFeatureStepDefinitions.java
@@ -31,10 +31,9 @@ public class AssignmentFeatureStepDefinitions {
   String error;
 
   /**
-   * Creating the climbsafe system with the desired number of weeks, date and price of guide per
-   * week
-   * @author  Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph Nassar
-   * @param dataTable The table that contains the inputs specified in the feature file
+   * 
+   * @author 
+   * @param dataTable
    */
   @Given("the following ClimbSafe system exists:")
   public void the_following_climb_safe_system_exists(io.cucumber.datatable.DataTable dataTable) {
@@ -59,14 +58,6 @@ public class AssignmentFeatureStepDefinitions {
     }
   }
 
-  /**
-   * Adding the pieces of equipment that should already be in the system before going into the when
-   * clause
-   * 
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param dataTable The table that contains the inputs specified in the feature file
-   */
   @Given("the following pieces of equipment exist in the system:")
   public void the_following_pieces_of_equipment_exist_in_the_system(
       io.cucumber.datatable.DataTable dataTable) {
@@ -84,14 +75,7 @@ public class AssignmentFeatureStepDefinitions {
     }
 
   }
-  
-  /**
-   * Adding the bundles that should already be in the system before going into the when clause
-   * 
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param dataTable The table that contains the inputs specified in the feature file
-   */
+
   @Given("the following equipment bundles exist in the system:")
   public void the_following_equipment_bundles_exist_in_the_system(
       io.cucumber.datatable.DataTable dataTable) {
@@ -123,14 +107,6 @@ public class AssignmentFeatureStepDefinitions {
 
   }
 
-  /**
-   * Adding the guides that should already be in the system before going into the when clause
-   * 
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param dataTable The table that contains the inputs specified in the feature file
-   * 
-   */
   @Given("the following guides exist in the system:")
   public void the_following_guides_exist_in_the_system(io.cucumber.datatable.DataTable dataTable) {
 
@@ -149,15 +125,6 @@ public class AssignmentFeatureStepDefinitions {
     }
   }
 
-  /**
-   * Adding the members that should already be in the system before going into the when clause
-   * 
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param dataTable The table that contains the inputs specified in the feature file
-   * 
-   */
-  
   @Given("the following members exist in the system:")
   public void the_following_members_exist_in_the_system(io.cucumber.datatable.DataTable dataTable) {
 
@@ -188,15 +155,6 @@ public class AssignmentFeatureStepDefinitions {
     }
   }
 
-  
-  
-  /**
-   * Calls the controller to try and initiate the assignment
-   * 
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   */
-  
   @When("the administrator attempts to initiate the assignment process")
   public void the_administrator_attempts_to_initiate_the_assignment_process() {
     try {
@@ -206,14 +164,6 @@ public class AssignmentFeatureStepDefinitions {
     }
   }
 
-  /**
-   * Checks that the assignments in the system are as expected
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   *         
-   * @param dataTable The table that contains the inputs specified in the feature file
-   */
-  
   @Then("the following assignments shall exist in the system:")
   public void the_following_assignments_shall_exist_in_the_system(
       io.cucumber.datatable.DataTable dataTable) {
@@ -222,7 +172,6 @@ public class AssignmentFeatureStepDefinitions {
 
     for (var row : rows) {
 
-   // Extracting the components of the table
       String memberEmail = row.get("memberEmail");
       String guideEmail = row.get("guideEmail");
       int startWeek = Integer.parseInt(row.get("startWeek"));
@@ -232,12 +181,11 @@ public class AssignmentFeatureStepDefinitions {
       assertNotNull(user); // Checking it is not null
       assertTrue(user instanceof Member); // Checking that it is a member
 
-      //Get assignment from user
-      Member member = (Member) user;    
-      Assignment assignment = member.getAssignment();   
+      Member member = (Member) user;
+      Assignment assignment = member.getAssignment();
 
       if (guideEmail == null) {
-        assertNull(assignment.getGuide());  
+        assertNull(assignment.getGuide());
       } else {
         assertEquals(guideEmail, assignment.getGuide().getEmail());
       }
@@ -249,58 +197,29 @@ public class AssignmentFeatureStepDefinitions {
 
   }
 
-  /**
-   * Checks that the state of the member in the system are as expected
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param memberEmail The member email
-   * @param assignmentState The state of the member
-   */
-  
-  
   @Then("the assignment for {string} shall be marked as {string}")
   public void the_assignment_for_shall_be_marked_as(String memberEmail, String assignmentState) {
-    Member member = (Member) User.getWithEmail(memberEmail);  //Getting the member
-    assertEquals(assignmentState, member.getAssignment().getAssignmentStateFullName()); //Making sure the state is correct
+    Member member = (Member) User.getWithEmail(memberEmail);
+    assertEquals(assignmentState, member.getAssignment().getAssignmentStateFullName());
   }
 
-  /**
-   * Checks that the number of assignment in the system are as expected
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param expectedNumberOfAssignments The expected number of assignments
-   */
   @Then("the number of assignments in the system shall be {string}")
   public void the_number_of_assignments_in_the_system_shall_be(String expectedNumberOfAssignments) {
-    assertEquals(Integer.parseInt(expectedNumberOfAssignments), climbSafe.getAssignments().size()); //Making sure the number of assignment is correct
+    assertEquals(Integer.parseInt(expectedNumberOfAssignments), climbSafe.getAssignments().size());
   }
 
-  /**
-   * Checks that the error raised in the system is as expected
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param expectedError The expected error
-   */
-  
   @Then("the system shall raise the error {string}")
   public void the_system_shall_raise_the_error(String expectedError) {
-    assertEquals(expectedError, error);  //Making sure that the error raised is correct
+    assertEquals(expectedError, error);
   }
 
-  /**
-   * Adding the assignments that should already be in the system
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param dataTable The table that contains the inputs specified in the feature file
-   */
-  
   @Given("the following assignments exist in the system:")
   public void the_following_assignments_exist_in_the_system(
       io.cucumber.datatable.DataTable dataTable) {
 
     List<Map<String, String>> rows = dataTable.asMaps();
 
-    // Extracting the components of the table
+    // Extracting the components of the table and adding the corresponding pieces of equipment
     for (var row : rows) {
 
       String memberEmail = row.get("memberEmail");
@@ -321,15 +240,6 @@ public class AssignmentFeatureStepDefinitions {
 
   }
 
-  /**
-   * Try to pay for a trip for a select member
-   * 
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param memberEmail The member email
-   * @param authorizationCode The authorization code
-   */
-  
   @When("the administrator attempts to confirm payment for {string} using authorization code {string}")
   public void the_administrator_attempts_to_confirm_payment_for_using_authorization_code(
       String memberEmail, String authorizationCode) {
@@ -340,14 +250,6 @@ public class AssignmentFeatureStepDefinitions {
     }
   }
 
-  /**
-   * Checks that the authorization code in the system are as expected
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param memberEmail The member email
-   * @param authorizationCode The authorization code
-   */  
-  
   @Then("the assignment for {string} shall record the authorization code {string}")
   public void the_assignment_for_shall_record_the_authorization_code(String memberEmail,
       String authorizationCode) {
@@ -355,50 +257,22 @@ public class AssignmentFeatureStepDefinitions {
     assertEquals(authorizationCode, member.getAssignment().getAuthorizationCode());
   }
 
-  /**
-   * Checks that the member is not in the system, as expected
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param memberEmail The member email
-   */
   @Then("the member account with the email {string} does not exist")
   public void the_member_account_with_the_email_does_not_exist(String memberEmail) {
     User user = User.getWithEmail(memberEmail);
     assertTrue(user == null || user instanceof Guide);
   }
 
-  
-  /**
-   * Checks that the number of members in the system are as expected
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param numberOfMembers The number of members
-   */
   @Then("there are {string} members in the system")
   public void there_are_members_in_the_system(String numberOfMembers) {
     assertEquals(Integer.parseInt(numberOfMembers), climbSafe.getMembers().size());
   }
 
-  
-  /**
-   * Checks that the error raised in the system is as expected
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param expectedError The expected error
-   */
   @Then("the error {string} shall be raised")
   public void the_error_shall_be_raised(String expectedError) {
     assertEquals(expectedError, error);
   }
 
-  
-  /**
-   * Try to cancel a trip for a select member
-   * 
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param memberEmail The member email
-   */
   @When("the administrator attempts to cancel the trip for {string}")
   public void the_administrator_attempts_to_cancel_the_trip_for(String memberEmail) {
     try {
@@ -408,25 +282,12 @@ public class AssignmentFeatureStepDefinitions {
     }
   }
 
- /**
-  * Setting the state of a member as paid
-  * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-  *         Nassar
-  * @param memberEmail The member email
-  */
   @Given("the member with {string} has paid for their trip")
   public void the_member_with_has_paid_for_their_trip(String memberEmail) {
     Member member = (Member) User.getWithEmail(memberEmail);
     member.getAssignment().setState(AssignmentState.Paid);
   }
 
-  /**
-   * Checks that the refund in the system for the member is as expected
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param memberEmail The member email
-   * @param refund The refund 
-   */
   @Then("the member with email address {string} shall receive a refund of {string} percent")
   public void the_member_with_email_address_shall_receive_a_refund_of_percent(String memberEmail,
       String refund) {
@@ -437,24 +298,12 @@ public class AssignmentFeatureStepDefinitions {
     assertEquals(Integer.parseInt(refund), ((Member) user).getAssignment().getRefundPercentage());
   }
 
-  /**
-   * Checks that the member in the system has started their trip
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param memberEmail The member email
-   */
   @Given("the member with {string} has started their trip")
   public void the_member_with_has_started_their_trip(String memberEmail) {
     Member member = (Member) User.getWithEmail(memberEmail);
     member.getAssignment().setState(AssignmentState.Started);
   }
 
-  /**
-   * Try and cancel the trip for select member
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param memberEmail The member email
-   */
   @When("the administrator attempts to finish the trip for the member with email {string}")
   public void the_administrator_attempts_to_finish_the_trip_for_the_member_with_email(
       String memberEmail) {
@@ -464,12 +313,6 @@ public class AssignmentFeatureStepDefinitions {
       error = e.getMessage();
     }
   }
-  /**
-   * Checks that the member in the system is banned
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param memberEmail The member email
-   */
 
   @Given("the member with {string} is banned")
   public void the_member_with_is_banned(String memberEmail) {
@@ -477,13 +320,6 @@ public class AssignmentFeatureStepDefinitions {
     member.getAssignment().setState(AssignmentState.Banned);
   }
 
-  /**
-   * Checks that the state of the member in the system is as expected
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param memberEmail The member email
-   * @param assignmentState The state of the member
-   */
   @Then("the member with email {string} shall be {string}")
   public void the_member_with_email_shall_be(String memberEmail, String assignmentState) {
 
@@ -494,12 +330,6 @@ public class AssignmentFeatureStepDefinitions {
     assertEquals(assignmentState, ((Member) user).getAssignment().getAssignmentStateFullName());
   }
 
-  /**
-   * Try and start the trip with select number of week
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param weekNr The number of week
-   */
   @When("the administrator attempts to start the trips for week {string}")
   public void the_administrator_attempts_to_start_the_trips_for_week(String weekNr) {
     try {
@@ -509,24 +339,12 @@ public class AssignmentFeatureStepDefinitions {
     }
   }
 
-  /**
-   * Checks that the member in the system has cancelled their trip
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param memberEmail The member email
-   */
   @Given("the member with {string} has cancelled their trip")
   public void the_member_with_has_cancelled_their_trip(String memberEmail) {
     Member member = (Member) User.getWithEmail(memberEmail);
     member.getAssignment().setState(AssignmentState.Cancelled);
   }
 
-  /**
-   * Checks that the member in the system has finished their trip
-   * @author Wassim Jabbour, Matthieu Hakim, Karl Rouhana, Tinetendo Makata, Adam Kazma, Ralph
-   *         Nassar
-   * @param memberEmail The member email
-   */
   @Given("the member with {string} has finished their trip")
   public void the_member_with_has_finished_their_trip(String memberEmail) {
     Member member = (Member) User.getWithEmail(memberEmail);
