74c9ed52fad75a987431324e4e617cec8b8f7d7f
Matthieuhakim <77667703+Matthieuhakim@users.noreply.github.com>
2021-11-19 20:35:37 -0500

    addHotel controller method

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet7Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet7Controller.java
@@ -1,10 +1,74 @@
 package ca.mcgill.ecse.climbsafe.controller;
 
+import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
+import ca.mcgill.ecse.climbsafe.model.*;
+import ca.mcgill.ecse.climbsafe.model.Hotel.HotelRating;
+import ca.mcgill.ecse.climbsafe.persistence.ClimbSafePersistence;
+
 // We did not implement this feature set since we have 6 members on our team
 public class ClimbSafeFeatureSet7Controller {
 
   public static void addHotel(String name, String address, int nrStars)
-      throws InvalidInputException {}
+      throws InvalidInputException {
+    
+ // Checking for invalid name input
+    if (name.trim().equals("")) {
+      throw new InvalidInputException("Name cannot be empty");
+    }
+
+    // Checks for invalid address input
+    if (address.trim().equals("")) {
+      throw new InvalidInputException("Address cannot be empty");
+    }
+
+    // Checks to see if hotel with same name already exists in the system and outputs an error
+    // if that is the case
+    Hotel existingHotel = Hotel.getWithName(name);
+
+    if (existingHotel != null) {
+
+      throw new InvalidInputException("Hotel already exists in the system ");
+
+    }
+    // Putting the system we're working on in a local variable
+    ClimbSafe system = ClimbSafeApplication.getClimbSafe();
+
+    // Add the hotel to the system
+    system.addHotel(new Hotel(name, address, intToRating(nrStars), system));
+
+    ClimbSafePersistence.save(system);
+    
+  }
+  
+  
+  
+  
+  
   public static void updateHotel(String oldName, String newName, String newAddress, int newNrStars)
       throws InvalidInputException {}
+  
+  
+  
+  private static HotelRating intToRating(int rating) throws InvalidInputException {
+
+    switch (rating) {
+
+      case 1:
+        return HotelRating.OneStar;
+
+      case 2:
+        return HotelRating.TwoStars;
+
+      case 3:
+        return HotelRating.ThreeStars;
+
+      case 4:
+        return HotelRating.FourStars;
+      case 5:
+        return HotelRating.FiveStars;
+
+      default:
+        throw new InvalidInputException("Number of stars must be between 1 and 5");
+    }
+  }
 }
