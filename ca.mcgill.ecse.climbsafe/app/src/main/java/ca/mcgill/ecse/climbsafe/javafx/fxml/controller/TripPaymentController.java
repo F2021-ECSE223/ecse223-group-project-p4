3b8dfff0dba8e738c2e60e33393f6f04b017b6ec
Matthieuhakim <77667703+Matthieuhakim@users.noreply.github.com>
2021-11-22 14:12:45 -0500

    Pay for trip UI + controller

---
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/javafx/fxml/controller/TripPaymentController.java
@@ -0,0 +1,31 @@
+package ca.mcgill.ecse.climbsafe.javafx.fxml.controller;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TextField;
+
+import javafx.event.ActionEvent;
+
+import ca.mcgill.ecse.climbsafe.controller.AssignmentController;
+
+public class TripPaymentController {
+  @FXML
+  private Button payButtton;
+  @FXML
+  private TextField authCodeTextField;
+  @FXML
+  private TextField memberEmailTextField;
+
+  // Event Listener on Button[#payButtton].onAction
+  @FXML
+  public void payForTrip(ActionEvent event) {
+
+    try {
+      AssignmentController.payForTrip(memberEmailTextField.getText(), authCodeTextField.getText());
+    } catch (Exception e) {
+      ViewUtils.showError(e.getMessage());
+    }
+  }
+}
