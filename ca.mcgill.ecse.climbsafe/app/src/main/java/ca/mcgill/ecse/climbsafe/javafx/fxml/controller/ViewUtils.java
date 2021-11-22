c90a6aed625c26c540461283708f982b3efa98b9
Adam Kazma <adkazma2@gmail.com>
2021-11-21 21:57:11 -0500

    Equipment UI + Controller, showError method, + small fixes

---
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/javafx/fxml/controller/ViewUtils.java
@@ -0,0 +1,37 @@
+package ca.mcgill.ecse.climbsafe.javafx.fxml.controller;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class ViewUtils {
+  public static void makePopupWindow(String title, String message) {
+    Stage dialog = new Stage();
+    dialog.initModality(Modality.APPLICATION_MODAL);
+    VBox dialogPane = new VBox();
+
+    // create UI elements
+    Text text = new Text(message);
+    Button okButton = new Button("OK");
+    okButton.setOnAction(a -> dialog.close());
+
+    // display the popup window
+    int innerPadding = 10; // inner padding/spacing
+    int outerPadding = 100; // outer padding
+    dialogPane.setSpacing(innerPadding);
+    dialogPane.setAlignment(Pos.CENTER);
+    dialogPane.setPadding(new Insets(innerPadding, innerPadding, innerPadding, innerPadding));
+    dialogPane.getChildren().addAll(text, okButton);
+    Scene dialogScene = new Scene(dialogPane, outerPadding + 5 * message.length(), outerPadding);
+    dialog.setScene(dialogScene);
+    dialog.setTitle(title);
+    dialog.show();
+  }
+  public static void showError(String message) {
+    makePopupWindow("Error", message);
+  }
+}
