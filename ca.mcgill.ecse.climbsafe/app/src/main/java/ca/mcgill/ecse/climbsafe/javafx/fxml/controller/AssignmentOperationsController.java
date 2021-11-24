dbf19f1961169f93f811b83eb73b06b40f36bce5
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2021-11-24 11:57:19 -0500

    View assignments table

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/javafx/fxml/controller/AssignmentOperationsController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/javafx/fxml/controller/AssignmentOperationsController.java
@@ -1,39 +1,59 @@
 package ca.mcgill.ecse.climbsafe.javafx.fxml.controller;
 
-import javafx.fxml.FXML;
-
-import javafx.scene.control.Button;
+import java.util.ArrayList;
 import java.util.List;
 import ca.mcgill.ecse.climbsafe.controller.AssignmentController;
 import ca.mcgill.ecse.climbsafe.controller.ClimbSafeFeatureSet6Controller;
 import ca.mcgill.ecse.climbsafe.controller.InvalidInputException;
 import ca.mcgill.ecse.climbsafe.controller.TOAssignment;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class AssignmentOperationsController {
-	@FXML
-	private Button initiateAssignment;
-	@FXML
-	private TableView assignmentTable;
+  @FXML
+  private Button initiateAssignment;
+  @FXML
+  private TableView assignmentTable;
 
-	// Event Listener on Button[#initiateAssignment].onAction
-	@FXML
-	public void initiateSeason(ActionEvent event) {
-	  try {
-	    AssignmentController.initiateAssignment();
-	  }
-	  catch(InvalidInputException e) {
-        ViewUtils.showError(e.getMessage());
-        return;
-      }
-	}
-	
-	public void refreshTable() {
-	    
-	  List<TOAssignment> assignments = ClimbSafeFeatureSet6Controller.getAssignments();
-	  
-	  //TODO continue refresh table method
-	}  
+  private String[] TOAssignmentProperties =
+      {"memberEmail", "memberName", "guideEmail", "guideName", "hotelName", "startWeek", "endWeek",
+          "totalCostForGuide", "totalCostForEquipment", "status", "authorizationCode",
+          "refundedPercentageAmount", "climbRating", "climbComment", "climbLength"};
+
+  // Event Listener on Button[#initiateAssignment].onAction
+  @FXML
+  public void initiateSeason(ActionEvent event) {
+    try {
+      AssignmentController.initiateAssignment();
+    } catch (InvalidInputException e) {
+      ViewUtils.showError(e.getMessage());
+      return;
+    }
+  }
+
+  public void refresh() {
+
+    ObservableList<TOAssignment> data = FXCollections.observableArrayList();
+    List<TOAssignment> assignments = ClimbSafeFeatureSet6Controller.getAssignments();
+
+    for (TOAssignment assignment : assignments) {
+      data.add(assignment);
+    }
+
+    for (int i = 0; i < assignmentTable.getColumns().size(); i++) {
+      assignmentTable.getVisibleLeafColumn(i).setCellValueFactory(
+          new PropertyValueFactory<TOAssignment, String>(TOAssignmentProperties[i]));
+    }
+    
+    assignmentTable.setItems(data);
+
+
+
+  }
+
 }
--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/javafx/fxml/controller/ClimbingPathOperationsController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/javafx/fxml/controller/ClimbingPathOperationsController.java
@@ -9,85 +9,91 @@ import ca.mcgill.ecse.climbsafe.controller.ExtraFeaturesController;
 import javafx.event.ActionEvent;
 
 public class ClimbingPathOperationsController {
-	@FXML
-	private Button addNewPath;
-	@FXML
-	private TextField newPathName;
-	@FXML
-	private TextField newPathDifficulty;
-	@FXML
-	private TextField newPathDistance;
-	@FXML
-	private Button updateExistingPath;
-	@FXML
-	private TextField updatedPathDifficulty;
-	@FXML
-	private TextField updatedPathDistance;
-	@FXML
-	private TextField updatedPathName;
-	@FXML
-	private TextField oldPathName;
-	@FXML
-	private Button deleteExistingPath;
-	@FXML
-	private TextField pathToDelete;
+  @FXML
+  private Button addNewPath;
+  @FXML
+  private TextField newPathName;
+  @FXML
+  private TextField newPathDifficulty;
+  @FXML
+  private TextField newPathDistance;
+  @FXML
+  private Button updateExistingPath;
+  @FXML
+  private TextField updatedPathDifficulty;
+  @FXML
+  private TextField updatedPathDistance;
+  @FXML
+  private TextField updatedPathName;
+  @FXML
+  private TextField oldPathName;
+  @FXML
+  private Button deleteExistingPath;
+  @FXML
+  private TextField pathToDelete;
 
-	// Event Listener on Button[#addNewPath].onAction
-	@FXML
-	public void AddPath(ActionEvent event) {
-		
-	  
-	  if (!ViewUtils.isAlpha(newPathName.getText())|| !ViewUtils.isAlpha(newPathDifficulty.getText())){
-	    ViewUtils.showError("The input must only contain letters.");
-	    return;
-	  }
-	  try { 
-	    Integer.parseInt(newPathDistance.getText());
-	  }catch(Exception e){
-	    ViewUtils.showError("The path distance can only be a number.");
-	    return;
-	  }
-	  
-	  try {
-	    ExtraFeaturesController.addClimbingPath(newPathName.getText(), Integer.parseInt(newPathDistance.getText()), newPathDifficulty.getText());
-	  }catch(Exception e) {
-	    ViewUtils.showError(e.getMessage());
-	  }
-	}
-	// Event Listener on Button[#updateExistingPath].onAction
-	@FXML
-	public void UpdatePath(ActionEvent event) {
-		
-	  if (!ViewUtils.isAlpha(oldPathName.getText())|| !ViewUtils.isAlpha(updatedPathDifficulty.getText()) ||!ViewUtils.isAlpha(updatedPathName.getText()) ){
-        ViewUtils.showError("The input must only contain letters.");
-        return;
-      }
-      try { 
-        Integer.parseInt(updatedPathDistance.getText());
-      }catch(Exception e){
-        ViewUtils.showError("The path distance can only be a number.");
-        return;
-      }
-	   try {
-	        ExtraFeaturesController.updateClimbingPath(oldPathName.getText(),updatedPathName.getText(), Integer.parseInt(updatedPathDistance.getText()), updatedPathDifficulty.getText());
-	      }catch(Exception e) {
-	        ViewUtils.showError(e.getMessage());
-	      }
-	    }
-	
-	// Event Listener on Button[#deleteExistingPath].onAction
-	@FXML
-	public void deletePath(ActionEvent event) {
-		
-	  if (!ViewUtils.isAlpha(pathToDelete.getText())){
-        ViewUtils.showError("The input must only contain letters.");
-        return;
-      }
-	   try {
-         ExtraFeaturesController.deleteClimbingPath(pathToDelete.getText());
-       }catch(Exception e) {
-         ViewUtils.showError(e.getMessage());
-       }
-     
-	}
+  // Event Listener on Button[#addNewPath].onAction
+  @FXML
+  public void AddPath(ActionEvent event) {
+
+
+    if (!ViewUtils.isAlpha(newPathName.getText())
+        || !ViewUtils.isAlpha(newPathDifficulty.getText())) {
+      ViewUtils.showError("The input must only contain letters.");
+      return;
+    }
+    try {
+      Integer.parseInt(newPathDistance.getText());
+    } catch (Exception e) {
+      ViewUtils.showError("The path distance can only be a number.");
+      return;
+    }
+
+    try {
+      ExtraFeaturesController.addClimbingPath(newPathName.getText(),
+          Integer.parseInt(newPathDistance.getText()), newPathDifficulty.getText());
+    } catch (Exception e) {
+      ViewUtils.showError(e.getMessage());
+    }
+  }
+
+  // Event Listener on Button[#updateExistingPath].onAction
+  @FXML
+  public void UpdatePath(ActionEvent event) {
+
+    if (!ViewUtils.isAlpha(oldPathName.getText())
+        || !ViewUtils.isAlpha(updatedPathDifficulty.getText())
+        || !ViewUtils.isAlpha(updatedPathName.getText())) {
+      ViewUtils.showError("The input must only contain letters.");
+      return;
+    }
+    try {
+      Integer.parseInt(updatedPathDistance.getText());
+    } catch (Exception e) {
+      ViewUtils.showError("The path distance can only be a number.");
+      return;
+    }
+    try {
+      ExtraFeaturesController.updateClimbingPath(oldPathName.getText(), updatedPathName.getText(),
+          Integer.parseInt(updatedPathDistance.getText()), updatedPathDifficulty.getText());
+    } catch (Exception e) {
+      ViewUtils.showError(e.getMessage());
+    }
+  }
+
+  // Event Listener on Button[#deleteExistingPath].onAction
+  @FXML
+  public void deletePath(ActionEvent event) {
+
+    if (!ViewUtils.isAlpha(pathToDelete.getText())) {
+      ViewUtils.showError("The input must only contain letters.");
+      return;
+    }
+    try {
+      ExtraFeaturesController.deleteClimbingPath(pathToDelete.getText());
+    } catch (Exception e) {
+      ViewUtils.showError(e.getMessage());
+    }
+
+  }
 }
